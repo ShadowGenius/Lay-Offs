1cb0313f4739586fa15ac53ed5904d5b3698e997
Epic-Mushroom <malroop1@outlook.com>
2026-02-18 12:36:42 -0800

    add NPCPersonality class, which has methods to randomly select an action,
    also includes all the different npc archetypes
    there are a lot of placeholders, for example the NextAction method just returns a string describing the task, but it would ideally return an actual object for an action once that is implemented

---
+++ Assets/Scripts/NPC/NPCPersonality.cs
@@ -0,0 +1,132 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class NPCPersonality
+{
+    public static NPCPersonality generic_model = new NPCPersonality("Generic Model", 50, 10, 40, 80, -0.6f, 20, 0.6f);
+    public static NPCPersonality evil_mastermind = new NPCPersonality("Evil Mastermind", 
+                                                               basicTaskChance: 25, 
+                                                               untargetedSabotageChance: 25, 
+                                                               targetedActionChance: 50, 
+                                                               baseTargetedSabotageChance: 100, 
+                                                               targetedSabotageFriendlinessMult: -0.4f, 
+                                                               baseHelpChance: 0, 
+                                                               helpFriendlinessMult: 0.4f);
+    public static NPCPersonality saint = new NPCPersonality("The Saint",
+                                                     basicTaskChance: 70,
+                                                     untargetedSabotageChance: 0,
+                                                     targetedActionChance: 30,
+                                                     baseTargetedSabotageChance: 60,
+                                                     targetedSabotageFriendlinessMult: -0.6f,
+                                                     baseHelpChance: 40,
+                                                     helpFriendlinessMult: 0.6f);
+    public static NPCPersonality retaliator = new NPCPersonality("The Retaliator",
+                                                     basicTaskChance: 40,
+                                                     untargetedSabotageChance: 10,
+                                                     targetedActionChance: 50,
+                                                     baseTargetedSabotageChance: 100,
+                                                     targetedSabotageFriendlinessMult: -0.8f,
+                                                     baseHelpChance: 0,
+                                                     helpFriendlinessMult: 0.8f);
+    public static NPCPersonality madman = new NPCPersonality("The Madman",
+                                                     basicTaskChance: 20,
+                                                     untargetedSabotageChance: 30,
+                                                     targetedActionChance: 50,
+                                                     baseTargetedSabotageChance: 60,
+                                                     targetedSabotageFriendlinessMult: -0.2f,
+                                                     baseHelpChance: 40,
+                                                     helpFriendlinessMult: 0.2f);
+
+    // this class should be used as an attribute for a broader NPC class
+    public string name = "Generic Model";
+
+    private int basicTaskChance = 50;
+    private int untargetedSabotageChance = 10;
+    private int targetedActionChance = 40;
+
+    private int baseTargetedSabotageChance = 80;
+    private double targetedSabotageFriendlinessMult = -0.6f;
+
+    private int baseHelpChance = 20;
+    private double helpFriendlinessMult = 0.6f;
+
+    public NPCPersonality(string name, int basicTaskChance, int untargetedSabotageChance, int targetedActionChance,
+                          int baseTargetedSabotageChance, double targetedSabotageFriendlinessMult, int baseHelpChance,
+                          double helpFriendlinessMult)
+    {
+        this.name = name;
+        this.basicTaskChance = basicTaskChance;
+        this.untargetedSabotageChance = untargetedSabotageChance;
+        this.targetedActionChance = targetedActionChance;
+        this.baseTargetedSabotageChance = baseTargetedSabotageChance;
+        this.targetedSabotageFriendlinessMult = targetedSabotageFriendlinessMult;
+        this.baseHelpChance = baseHelpChance;
+        this.helpFriendlinessMult = helpFriendlinessMult;
+    }
+
+    double TargetedSabotageChance(double friendlinessToTarget)
+    {
+        // the broader NPC class should have an array of friendliness values for each other NPC + player, which this uses
+        return baseTargetedSabotageChance + (friendlinessToTarget * targetedSabotageFriendlinessMult);
+    }
+
+    double HelpChance(double friendlinessToTarget)
+    {
+        return baseHelpChance + (friendlinessToTarget * helpFriendlinessMult);
+    }
+
+    public string NextAction(double friendlinessToTarget) // might be a good idea to include the target as a parameter once that is implemented
+    {
+        // strings are just placeholders for the actual actions
+        // probably should return an object that tasks, sabotages, etc inherit from
+
+        double actionRoll = Random.Range(0f, 100f);
+        string action = "";
+
+        if (actionRoll < basicTaskChance)
+        {
+            action = "basic task";
+
+        } else if (actionRoll < basicTaskChance + untargetedSabotageChance)
+        {
+            action = "untargeted sabotage";
+
+        } else if (actionRoll < basicTaskChance + untargetedSabotageChance + targetedActionChance) // should just be 100 
+        {
+            double targetedActionRoll = Random.Range(0f, 100f);
+            string targetedAction = "";
+
+            if (targetedActionRoll < TargetedSabotageChance(friendlinessToTarget))
+            {
+                targetedAction = "targeted sabotage";
+            }
+            else 
+            {
+                targetedAction = "help";
+            }
+
+            action = targetedAction;
+        }
+
+        if (action == "basic task")
+        {
+            // select a random basic task from a list of basic tasks
+
+        } else if (action == "untargeted sabotage")
+        {
+            // select random untargeted sabotage
+
+        } else if (action == "targeted sabotage")
+        {
+            // ...
+
+        } else if (action == "help")
+        {
+            // ...
+
+        }
+
+        return action;
+    }
+
+}
---
+++ Assets/Scripts/Test Scripts/NPCPersonalityTest.cs
@@ -0,0 +1,40 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCPersonalityTest : MonoBehaviour
+{
+    private NPCPersonality[] personalities = {NPCPersonality.generic_model, NPCPersonality.evil_mastermind,
+    NPCPersonality.saint, NPCPersonality.retaliator, NPCPersonality.madman};
+
+    public int testsPerPersonality = 50;
+    public int friendliness = 50;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log($"Friendliness: {friendliness}");
+
+        for (int j = 0; j < personalities.Length; j++)
+        {
+            List<string> actions = new List<string>();
+
+            Debug.Log($"{personalities[j].name}");
+            for (int i = 1; i <= testsPerPersonality; ++i)
+            {
+                string action = personalities[j].NextAction(50);
+
+                actions.Add(action);
+                Debug.Log($"Test {i}/{testsPerPersonality}: {action}");
+            }
+
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
