7d194662dbc3720d6c477919af4ac7eb46bac2ca
Epic-Mushroom <malroop1@outlook.com>
2026-02-26 22:17:13 -0800

    add ManagerDesk

---
+++ Assets/Scripts/Objects/ManagerDesk.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class ManagerDesk : ObjectInteraction
+{
+    // communal printer, no one owns it (since it is related to untargeted sabotage)
+    public override void OnPlayerUse()
+    {
+
+        Debug.Log("Messing with the manager's desk");
+
+        Delivery deliveryTask = player.playerActions.Find(action => action is Delivery && action.IsNotFinished()) as Delivery;
+        
+        if (deliveryTask != null)
+        {
+            deliveryTask.MakeProgress();
+
+            Debug.Log($"Player made progress on delivery ({deliveryTask.PercentComplete()}% complete)");
+        }
+    }
+
+    public override void OnNPCUse(NPC npc)
+    {
+        Debug.Log("NPC using manager's desk");
+    }
+
+    public override void OnPlayerSabotage()
+    {
+        // can't sabotage the manager's desk
+    }
+
+    public override void OnNPCSabotage(NPC npc)
+    {
+        // can't sabotage the manager's desk
+    }
+}
