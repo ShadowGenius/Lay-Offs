1eca586885e280ac36ded2cbc677537215df7ed6
Epic-Mushroom <malroop1@outlook.com>
2026-03-08 11:42:12 -0700

    player can now give water to npcs
    can only be done once per npc, and it increases the friendliness of the npc towards the player

--- Assets/Scripts/Actions/Help.cs
+++ Assets/Scripts/Actions/Help.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Help : TargetedAction
 {
+    public static int friendlinessIncrease = 0;
     public Help(Character character, Character target) : base(character, target)
     {
 
@@ -10,6 +11,8 @@ public class Help : TargetedAction
 
 public class GiveCupOfWater : Help
 {
+    public static int friendlinessIncrease = 10;
+
     public GiveCupOfWater(Character character, Character target) : base(character, target)
     {
         this.title = $"Give a Cup of Water to {target.name}";
@@ -18,6 +21,8 @@ public class GiveCupOfWater : Help
 
 public class PickUpItems : Help
 {
+    public static int friendlinessIncrease = 5;
+
     public PickUpItems(Character character, Character target) : base(character, target)
     {
         this.title = $"Pick Up {target.name}'s Dropped Items";
@@ -26,6 +31,8 @@ public class PickUpItems : Help
 
 public class FixComputer : Help
 {
+    public static int friendlinessIncrease = 15;
+
     public FixComputer(Character character, Character target) : base(character, target)
     {
         this.title = $"Fix {target.name}'s Computer";
