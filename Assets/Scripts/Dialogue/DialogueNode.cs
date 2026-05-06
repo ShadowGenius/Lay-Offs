2ddd300cec20ed27151a9c7d56f2b95ee267758e
tinamengxq <tinam5@uci.edu>
2026-05-06 15:31:20 -0700

    branching dialogue

--- Assets/Scripts/Dialogue/DialogueNode.cs
+++ Assets/Scripts/Dialogue/DialogueNode.cs
@@ -1,9 +1,21 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 [CreateAssetMenu(fileName = "DialogueNode", menuName = "Scriptable Objects/Dialogue Node")]
 public class DialogueNode : ScriptableObject
 {
     [TextArea(2,4)]
     public string[] lines;
+    public string speakerName;
+    public bool hasChoice;
+    public List<DialogueChoice> choices = new List<DialogueChoice>();
+    public DialogueNode nextNode;
 
+}
+
+[System.Serializable]
+public class DialogueChoice
+{
+    public string label;
+    public DialogueNode nextNode;
 }
